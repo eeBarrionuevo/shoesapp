70afafb84d55ff3b51677ef9f92263621ed03c85
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-23 21:39:03 -0500

    Mods

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -13,77 +13,92 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            color: BrandColor.primaryColor,
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              color: BrandColor.primaryColor,
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    spacing40,
+                    H1(
+                      text: "Hey José Daniel Diaz, bievenido nuevamente",
+                      height: 1.15,
+                    ),
+                    spacing8,
+                    H5(
+                      text: "Tenemos las mejores ofertas siempre",
+                    ),
+                    spacing16,
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      physics: const BouncingScrollPhysics(),
+                      child: Row(
+                        children: [
+                          ItemOfferWidget(),
+                          ItemOfferWidget(),
+                          ItemOfferWidget(),
+                        ],
+                      ),
+                    ),
+                    spacing4,
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  spacing40,
-                  H1(
-                    text: "Hey José Daniel Diaz, bievenido nuevamente",
-                    height: 1.15,
-                  ),
-                  spacing8,
                   H5(
-                    text: "Tenemos las mejores ofertas siempre",
+                    text: "Últimos ingresos",
+                    fontWeight: FontWeight.w700,
                   ),
-                  spacing16,
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    physics: const BouncingScrollPhysics(),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: BrandColor.primaryColor.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
                     child: Row(
                       children: [
-                        ItemOfferWidget(),
-                        ItemOfferWidget(),
-                        ItemOfferWidget(),
+                        H6(
+                          text: "Ver todo",
+                          color: BrandColor.secondaryFontColor,
+                        ),
+                        const Icon(
+                          Icons.arrow_right_alt,
+                          size: 16.0,
+                          color: BrandColor.secondaryFontColor,
+                        )
                       ],
                     ),
                   ),
-                  spacing4,
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                H5(
-                  text: "Últimos ingresos",
-                  fontWeight: FontWeight.w700,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0,
-                    vertical: 4.0,
-                  ),
-                  decoration: BoxDecoration(
-                    color: BrandColor.primaryColor.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: Row(
-                    children: [
-                      H6(
-                        text: "Ver todo",
-                        color: BrandColor.secondaryFontColor,
-                      ),
-                      const Icon(
-                        Icons.arrow_right_alt,
-                        size: 16.0,
-                        color: BrandColor.secondaryFontColor,
-                      )
-                    ],
-                  ),
-                ),
-              ],
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemCount: 6,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.red,
+                );
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
