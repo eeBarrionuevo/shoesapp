d4b77539be82591c01d2cc6b746761da19e33165
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-24 22:27:45 -0500

    Mods

--- lib/ui/pages/brand_page.dart
+++ lib/ui/pages/brand_page.dart
@@ -1,36 +1,39 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:shoesappclient/ui/widgets/common_text.dart';
 import 'package:shoesappclient/ui/widgets/common_widget.dart';
+import 'package:shoesappclient/ui/widgets/item_brand_widget.dart';
 import 'package:shoesappclient/utils/asset_data.dart';
 
 class BrandPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 1.5,
-        children: [
-          Container(
-            color: Colors.red,
-            child: CachedNetworkImage(
-              imageUrl:
-                  "https://cooljsonline.com/wp-content/uploads/2020/05/shop-adidas-banner.jpg",
-              fit: BoxFit.cover,
-              errorWidget: (context, url, error) {
-                return Image.asset(AssetData.imagePlaceholder);
-              },
-              progressIndicatorBuilder: (context, url, progress) {
-                return loadingWidget;
-              },
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            floating: true,
+            title: H5(
+              text: "Nuestras marcas",
+              fontWeight: FontWeight.w700,
             ),
           ),
-          Container(
-            color: Colors.teal,
+          SliverGrid.count(
+            crossAxisCount: 2,
+            childAspectRatio: 1.5,
+            children: [
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+              ItemBrandWidget(),
+            ],
           ),
-          Container(
-            color: Colors.yellow,
-          )
         ],
       ),
     );
---
+++ lib/ui/widgets/item_brand_widget.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:shoesappclient/ui/widgets/common_widget.dart';
+import 'package:shoesappclient/utils/asset_data.dart';
+
+class ItemBrandWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // margin: const EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: CachedNetworkImage(
+          imageUrl:
+              "https://cooljsonline.com/wp-content/uploads/2020/05/shop-adidas-banner.jpg",
+          fit: BoxFit.cover,
+          errorWidget: (context, url, error) {
+            return Image.asset(AssetData.imagePlaceholder);
+          },
+          progressIndicatorBuilder: (context, url, progress) {
+            return loadingWidget;
+          },
+        ),
+      ),
+    );
+  }
+}
