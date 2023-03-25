f37ab08c46c44629748cdb921825b888fe5f99d9
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-24 20:02:18 -0500

    Mods

--- lib/ui/pages/explore_page.dart
+++ lib/ui/pages/explore_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shoesappclient/ui/general/brand_color.dart';
+import 'package:shoesappclient/ui/widgets/item_offer_widget.dart';
 import 'package:shoesappclient/ui/widgets/item_product_widget.dart';
 
 class ExplorePage extends StatelessWidget {
@@ -8,73 +11,130 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              color: BrandColor.primaryColor,
-              child: SafeArea(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      cursorColor: BrandColor.secondaryFontColor,
-                      style: const TextStyle(
-                        fontSize: 14.0,
-                        color: BrandColor.secondaryFontColor,
-                      ),
-                      // enabled: false,
-                      readOnly: true,
-                      decoration: InputDecoration(
-                        hintText: "Buscar producto...",
-                        hintStyle: const TextStyle(
-                          fontSize: 14.0,
-                          color: BrandColor.secondaryFontColor,
-                        ),
-                        filled: true,
-                        fillColor: Colors.white.withOpacity(0.35),
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          size: 18.0,
-                          color: BrandColor.secondaryFontColor,
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 0,
-                          vertical: 12.0,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide.none,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text("Hola"),
+            backgroundColor: Colors.black,
+            expandedHeight: 300,
+            floating: true,
+            pinned: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                "https://images.pexels.com/photos/14360308/pexels-photo-14360308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                fit: BoxFit.cover,
+              ),
+            ),
+            // bottom: PreferredSize(
+            //   preferredSize: Size(200, 70),
+            //   child: Expanded(
+            //     child: Container(
+            //       width: double.infinity,
+            //       color: Colors.amber,
+            //       child: Text("Hola"),
+            //     ),
+            //   ),
+            // ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 150,
+                  color: Colors.indigo,
                 ),
-              ),
+                Container(
+                  height: 150,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 150,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 150,
+                  color: Colors.indigo,
+                ),
+                Container(
+                  height: 150,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 150,
+                  color: Colors.green,
+                ),
+              ],
             ),
-            GridView.builder(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              physics: const ScrollPhysics(),
-              itemCount: 30,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 6.0,
-                crossAxisSpacing: 6.0,
-                childAspectRatio: 0.9,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return ItemProductWidget();
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       Container(
+      //         padding: const EdgeInsets.all(16.0),
+      //         color: BrandColor.primaryColor,
+      //         child: SafeArea(
+      //           child: Column(
+      //             mainAxisSize: MainAxisSize.min,
+      //             children: [
+      //               TextField(
+      //                 cursorColor: BrandColor.secondaryFontColor,
+      //                 style: const TextStyle(
+      //                   fontSize: 14.0,
+      //                   color: BrandColor.secondaryFontColor,
+      //                 ),
+      //                 // enabled: false,
+      //                 readOnly: true,
+      //                 decoration: InputDecoration(
+      //                   hintText: "Buscar producto...",
+      //                   hintStyle: const TextStyle(
+      //                     fontSize: 14.0,
+      //                     color: BrandColor.secondaryFontColor,
+      //                   ),
+      //                   filled: true,
+      //                   fillColor: Colors.white.withOpacity(0.35),
+      //                   prefixIcon: const Icon(
+      //                     Icons.search,
+      //                     size: 18.0,
+      //                     color: BrandColor.secondaryFontColor,
+      //                   ),
+      //                   contentPadding: const EdgeInsets.symmetric(
+      //                     horizontal: 0,
+      //                     vertical: 12.0,
+      //                   ),
+      //                   focusedBorder: OutlineInputBorder(
+      //                     borderRadius: BorderRadius.circular(30.0),
+      //                     borderSide: BorderSide.none,
+      //                   ),
+      //                   enabledBorder: OutlineInputBorder(
+      //                     borderRadius: BorderRadius.circular(30.0),
+      //                     borderSide: BorderSide.none,
+      //                   ),
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //       GridView.builder(
+      //         padding: EdgeInsets.zero,
+      //         shrinkWrap: true,
+      //         physics: const ScrollPhysics(),
+      //         itemCount: 30,
+      //         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //           crossAxisCount: 2,
+      //           mainAxisSpacing: 6.0,
+      //           crossAxisSpacing: 6.0,
+      //           childAspectRatio: 0.9,
+      //         ),
+      //         itemBuilder: (BuildContext context, int index) {
+      //           return ItemProductWidget();
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
