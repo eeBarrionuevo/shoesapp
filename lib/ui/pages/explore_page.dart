12762b3f3a1d4f774724da57269198d66100c7c2
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-24 21:02:46 -0500

    Mods

--- lib/ui/pages/explore_page.dart
+++ lib/ui/pages/explore_page.dart
@@ -11,104 +11,164 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: CustomScrollView(
+      //   slivers: [
+      //     SliverAppBar(
+      //       title: Text("Hola"),
+      //       backgroundColor: Colors.black,
+      //       expandedHeight: 180,
+      //       floating: true,
+      //       pinned: false,
+      //       flexibleSpace: FlexibleSpaceBar(
+      //         background: Image.network(
+      //           "https://images.pexels.com/photos/14360308/pexels-photo-14360308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      //           fit: BoxFit.cover,
+      //         ),
+      //       ),
+      //       // bottom: PreferredSize(
+      //       //   preferredSize: Size(200, 70),
+      //       //   child: Expanded(
+      //       //     child: Container(
+      //       //       width: double.infinity,
+      //       //       color: Colors.amber,
+      //       //       child: Text("Hola"),
+      //       //     ),
+      //       //   ),
+      //       // ),
+      //     ),
+      //     SliverList(
+      //       delegate: SliverChildListDelegate(
+      //         [
+      //           Container(
+      //             height: 150,
+      //             color: Colors.indigo,
+      //           ),
+      //           Container(
+      //             height: 150,
+      //             color: Colors.red,
+      //           ),
+      //           Container(
+      //             height: 150,
+      //             color: Colors.green,
+      //           ),
+      //           Container(
+      //             height: 150,
+      //             color: Colors.indigo,
+      //           ),
+      //           Container(
+      //             height: 150,
+      //             color: Colors.red,
+      //           ),
+      //           Container(
+      //             height: 150,
+      //             color: Colors.green,
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //     SliverGrid.count(
+      //       crossAxisCount: 2,
+      //       children: [
+      //         Container(
+      //           color: Colors.amber,
+      //         ),
+      //         Container(
+      //           color: Colors.teal,
+      //         ),
+      //         Container(
+      //           color: Colors.pink,
+      //         ),
+      //       ],
+      //     ),
+      //     SliverPadding(
+      //       padding: const EdgeInsets.all(20.0),
+      //       sliver: SliverGrid(
+      //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //           crossAxisCount: 2,
+      //           mainAxisSpacing: 10,
+      //           crossAxisSpacing: 10,
+      //         ),
+      //         delegate: SliverChildBuilderDelegate(
+      //           (BuildContext context, int index) {
+      //             return Container(
+      //               color: Colors.teal,
+      //             );
+      //           },
+      //           childCount: 10,
+      //         ),
+      //         // delegate: SliverChildListDelegate(
+      //         //   [
+      //         //     Container(
+      //         //       color: Colors.amber,
+      //         //     ),
+      //         //     Container(
+      //         //       color: Colors.teal,
+      //         //     ),
+      //         //     Container(
+      //         //       color: Colors.pink,
+      //         //     ),
+      //         //   ],
+      //         // ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            title: Text("Hola"),
-            backgroundColor: Colors.black,
-            expandedHeight: 180,
+            backgroundColor: BrandColor.primaryColor,
             floating: true,
             pinned: false,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.network(
-                "https://images.pexels.com/photos/14360308/pexels-photo-14360308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                fit: BoxFit.cover,
+            // expandedHeight: 300.0,
+
+            title: TextField(
+              cursorColor: BrandColor.secondaryFontColor,
+              style: const TextStyle(
+                fontSize: 14.0,
+                color: BrandColor.secondaryFontColor,
+              ),
+              // enabled: false,
+              readOnly: true,
+              decoration: InputDecoration(
+                hintText: "Buscar producto...",
+                hintStyle: const TextStyle(
+                  fontSize: 14.0,
+                  color: BrandColor.secondaryFontColor,
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.35),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  size: 18.0,
+                  color: BrandColor.secondaryFontColor,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 0,
+                  vertical: 12.0,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide.none,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide.none,
+                ),
               ),
             ),
-            // bottom: PreferredSize(
-            //   preferredSize: Size(200, 70),
-            //   child: Expanded(
-            //     child: Container(
-            //       width: double.infinity,
-            //       color: Colors.amber,
-            //       child: Text("Hola"),
-            //     ),
-            //   ),
-            // ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Container(
-                  height: 150,
-                  color: Colors.indigo,
-                ),
-                Container(
-                  height: 150,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 150,
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 150,
-                  color: Colors.indigo,
-                ),
-                Container(
-                  height: 150,
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 150,
-                  color: Colors.green,
-                ),
-              ],
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 6.0,
+              crossAxisSpacing: 6.0,
+              childAspectRatio: 0.9,
             ),
-          ),
-          SliverGrid.count(
-            crossAxisCount: 2,
-            children: [
-              Container(
-                color: Colors.amber,
-              ),
-              Container(
-                color: Colors.teal,
-              ),
-              Container(
-                color: Colors.pink,
-              ),
-            ],
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.all(20.0),
-            sliver: SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Container(
-                    color: Colors.teal,
-                  );
-                },
-                childCount: 10,
-              ),
-              // delegate: SliverChildListDelegate(
-              //   [
-              //     Container(
-              //       color: Colors.amber,
-              //     ),
-              //     Container(
-              //       color: Colors.teal,
-              //     ),
-              //     Container(
-              //       color: Colors.pink,
-              //     ),
-              //   ],
-              // ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return const ItemProductWidget();
+              },
+              childCount: 10,
             ),
           ),
         ],
