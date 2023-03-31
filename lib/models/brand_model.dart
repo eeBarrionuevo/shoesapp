e1a519aaf02dcbcca1ebc7bf4e8a86d1d1e1b8e0
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-30 23:08:38 -0500

    Mods

--- lib/models/brand_model.dart
+++ lib/models/brand_model.dart
@@ -2,21 +2,25 @@ class BrandModel {
   String? id;
   String name;
   bool status;
+  String image;
 
   BrandModel({
     this.id,
     required this.name,
     required this.status,
+    required this.image,
   });
 
   factory BrandModel.fromJson(Map<String, dynamic> json) => BrandModel(
         name: json["name"] ?? "",
         status: json["status"] ?? false,
+        image: json["image"] ?? "",
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "status": status,
+        "image": image,
       };
 }
