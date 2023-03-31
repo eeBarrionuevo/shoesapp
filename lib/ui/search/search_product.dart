612c15d99ecb93d424f3f5004c9706c441651eb5
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-30 21:54:52 -0500

    Mods

--- lib/ui/search/search_product.dart
+++ lib/ui/search/search_product.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shoesappclient/models/product_model.dart';
+import 'package:shoesappclient/ui/general/brand_color.dart';
+import 'package:shoesappclient/ui/widgets/common_text.dart';
+import 'package:shoesappclient/ui/widgets/common_widget.dart';
+import 'package:shoesappclient/utils/asset_data.dart';
+import 'package:shoesappclient/utils/calculate.dart';
 
 class SearchProduct extends SearchDelegate {
   List<ProductModel> products;
@@ -19,6 +25,42 @@ class SearchProduct extends SearchDelegate {
     "Elí",
     "Oliver"
   ];
+
+  @override
+  String get searchFieldLabel => "Buscar producto...";
+
+  @override
+  TextStyle get searchFieldStyle => TextStyle(
+        fontSize: 16.0,
+        color: BrandColor.primaryFontColor.withOpacity(0.80),
+      );
+
+  @override
+  InputDecorationTheme get searchFieldDecorationTheme => InputDecorationTheme(
+        hintStyle: TextStyle(
+          fontSize: 14.0,
+          color: BrandColor.primaryFontColor.withOpacity(0.6),
+        ),
+        filled: true,
+        fillColor: Colors.black.withOpacity(0.04),
+        // prefixIcon: const Icon(
+        //   Icons.search,
+        //   size: 18.0,
+        //   color: BrandColor.secondaryFontColor,
+        // ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12.0,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide.none,
+        ),
+      );
 
   @override
   List<Widget>? buildActions(BuildContext context) {
@@ -53,8 +95,79 @@ class SearchProduct extends SearchDelegate {
     return ListView.builder(
       itemCount: suggestions.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(suggestions[index].name),
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 8,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+              color: BrandColor.secondaryColor.withOpacity(0.4),
+            ),
+          ),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: CachedNetworkImage(
+                  imageUrl: suggestions[index].image,
+                  height: 90.0,
+                  width: 105,
+                  fit: BoxFit.cover,
+                  errorWidget: (context, url, error) {
+                    return Image.asset(
+                      AssetData.imagePlaceholder,
+                    );
+                  },
+                  progressIndicatorBuilder: (context, url, progress) {
+                    return loadingWidget;
+                  },
+                ),
+              ),
+              spacing12Width,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    H6(
+                      text: suggestions[index].brand,
+                      color: BrandColor.primaryFontColor.withOpacity(0.55),
+                    ),
+                    spacing2,
+                    H5(
+                      text: suggestions[index].name,
+                      height: 1.1,
+                      maxLines: 2,
+                      textOverflow: TextOverflow.ellipsis,
+                    ),
+                    spacing4,
+                    Row(
+                      children: [
+                        H5(
+                          text:
+                              "S/ ${Calculate.getPrice(suggestions[index].price, suggestions[index].discount).toStringAsFixed(2)}",
+                          fontWeight: FontWeight.w700,
+                        ),
+                        spacing8Width,
+                        suggestions[index].discount > 0
+                            ? H6(
+                                text:
+                                    "S/ ${suggestions[index].price.toStringAsFixed(2)}",
+                                color: BrandColor.primaryFontColor
+                                    .withOpacity(0.55),
+                                textDecoration: TextDecoration.lineThrough,
+                              )
+                            : const SizedBox(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
@@ -69,8 +182,79 @@ class SearchProduct extends SearchDelegate {
     return ListView.builder(
       itemCount: suggestions.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text(suggestions[index].name),
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 8,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12.0),
+            border: Border.all(
+              color: BrandColor.secondaryColor.withOpacity(0.4),
+            ),
+          ),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: CachedNetworkImage(
+                  imageUrl: suggestions[index].image,
+                  height: 90.0,
+                  width: 105,
+                  fit: BoxFit.cover,
+                  errorWidget: (context, url, error) {
+                    return Image.asset(
+                      AssetData.imagePlaceholder,
+                    );
+                  },
+                  progressIndicatorBuilder: (context, url, progress) {
+                    return loadingWidget;
+                  },
+                ),
+              ),
+              spacing12Width,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    H6(
+                      text: suggestions[index].brand,
+                      color: BrandColor.primaryFontColor.withOpacity(0.55),
+                    ),
+                    spacing2,
+                    H5(
+                      text: suggestions[index].name,
+                      height: 1.1,
+                      maxLines: 2,
+                      textOverflow: TextOverflow.ellipsis,
+                    ),
+                    spacing4,
+                    Row(
+                      children: [
+                        H5(
+                          text:
+                              "S/ ${Calculate.getPrice(suggestions[index].price, suggestions[index].discount).toStringAsFixed(2)}",
+                          fontWeight: FontWeight.w700,
+                        ),
+                        spacing8Width,
+                        suggestions[index].discount > 0
+                            ? H6(
+                                text:
+                                    "S/ ${suggestions[index].price.toStringAsFixed(2)}",
+                                color: BrandColor.primaryFontColor
+                                    .withOpacity(0.55),
+                                textDecoration: TextDecoration.lineThrough,
+                              )
+                            : const SizedBox(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
