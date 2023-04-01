4640edc84214ea60085cd3203e45b2013c552444
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-31 21:59:09 -0500

    Mods

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shoesappclient/ui/general/brand_color.dart';
+import 'package:shoesappclient/ui/widgets/common_input_widget.dart';
 import 'package:shoesappclient/ui/widgets/common_text.dart';
 import 'package:shoesappclient/ui/widgets/common_widget.dart';
 import 'package:shoesappclient/utils/asset_data.dart';
@@ -72,55 +73,7 @@ class LoginPage extends StatelessWidget {
                       color: BrandColor.primaryFontColor.withOpacity(0.80),
                     ),
                     spacing20,
-                    Container(
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.05),
-                            blurRadius: 12,
-                            offset: const Offset(4, 4),
-                          ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Tu correo electrónico",
-                          hintStyle: TextStyle(
-                            fontSize: 14.0,
-                            color:
-                                BrandColor.primaryFontColor.withOpacity(0.60),
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
-                          contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 16,
-                            vertical: 12,
-                          ),
-                          prefixIcon: SvgPicture.asset(
-                            AssetData.iconMail,
-                            fit: BoxFit.scaleDown,
-                            color:
-                                BrandColor.primaryFontColor.withOpacity(0.50),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                          errorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                          focusedErrorBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                    ),
+                    CommonInputWidget(),
                   ],
                 ),
               ),
---
+++ lib/ui/widgets/common_input_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shoesappclient/ui/general/brand_color.dart';
+import 'package:shoesappclient/utils/asset_data.dart';
+
+class CommonInputWidget extends StatelessWidget {
+  const CommonInputWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 12,
+                offset: const Offset(4, 4),
+              ),
+            ],
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: "Tu correo electrónico",
+              hintStyle: TextStyle(
+                fontSize: 14.0,
+                color: BrandColor.primaryFontColor.withOpacity(0.60),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 12,
+              ),
+              prefixIcon: SvgPicture.asset(
+                AssetData.iconMail,
+                fit: BoxFit.scaleDown,
+                color: BrandColor.primaryFontColor.withOpacity(0.50),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
