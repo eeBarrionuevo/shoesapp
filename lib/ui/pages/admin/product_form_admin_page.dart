63a5a31924f8b1d1f5279a4d2b5ba310b5ff4765
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-04-11 20:15:29 -0500

    Mods

--- lib/ui/pages/admin/product_form_admin_page.dart
+++ lib/ui/pages/admin/product_form_admin_page.dart
@@ -47,203 +47,219 @@ class _ProductFormAdminPageState extends State<ProductFormAdminPage> {
           fontWeight: FontWeight.bold,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CommonInputWidget(
-                label: "Nombre del producto",
-                hintText: "Nombre del producto",
-                icon: AssetData.iconNotepad,
-                controller: nameController,
-                inputType: InputTypeEnum.text,
-              ),
-              spacing20,
-              H5(
-                text: "  Marca:",
-                fontWeight: FontWeight.w500,
-              ),
-              spacing8,
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      blurRadius: 12,
-                      offset: const Offset(4, 4),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CommonInputWidget(
+                    label: "Nombre del producto",
+                    hintText: "Nombre del producto",
+                    icon: AssetData.iconNotepad,
+                    controller: nameController,
+                    inputType: InputTypeEnum.text,
+                  ),
+                  spacing20,
+                  H5(
+                    text: "  Marca:",
+                    fontWeight: FontWeight.w500,
+                  ),
+                  spacing8,
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.05),
+                          blurRadius: 12,
+                          offset: const Offset(4, 4),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: DropdownButton(
-                  value: "Adidas",
-                  isExpanded: true,
-                  borderRadius: BorderRadius.circular(20.0),
-                  elevation: 6,
-                  underline: const SizedBox(),
-                  items: [
-                    DropdownMenuItem(
-                      child: H5(text: "Adidas"),
+                    child: DropdownButton(
                       value: "Adidas",
-                    ),
-                    DropdownMenuItem(
-                      child: H5(text: "Nike"),
-                      value: "Nike",
-                    ),
-                    DropdownMenuItem(
-                      child: H5(text: "New Balance"),
-                      value: "New Balance",
-                    ),
-                  ],
-                  onChanged: (value) {},
-                ),
-              ),
-              spacing20,
-              Row(
-                children: [
-                  Expanded(
-                    child: CommonInputWidget(
-                      label: "Precio (S/.)",
-                      hintText: "Precio",
-                      icon: AssetData.iconDollar,
-                      controller: nameController,
-                      inputType: InputTypeEnum.text,
+                      isExpanded: true,
+                      borderRadius: BorderRadius.circular(20.0),
+                      elevation: 6,
+                      underline: const SizedBox(),
+                      items: [
+                        DropdownMenuItem(
+                          child: H5(text: "Adidas"),
+                          value: "Adidas",
+                        ),
+                        DropdownMenuItem(
+                          child: H5(text: "Nike"),
+                          value: "Nike",
+                        ),
+                        DropdownMenuItem(
+                          child: H5(text: "New Balance"),
+                          value: "New Balance",
+                        ),
+                      ],
+                      onChanged: (value) {},
                     ),
                   ),
-                  spacing16Width,
-                  Expanded(
-                    child: CommonInputWidget(
-                      label: "Descuento (%)",
-                      hintText: "Descuento",
-                      icon: AssetData.iconOffer,
-                      controller: nameController,
-                      inputType: InputTypeEnum.text,
-                    ),
-                  ),
-                ],
-              ),
-              spacing20,
-              Row(
-                children: [
-                  Expanded(
-                    child: CommonInputWidget(
-                      label: "Stock",
-                      hintText: "Stock",
-                      icon: AssetData.iconShape,
-                      controller: nameController,
-                      inputType: InputTypeEnum.text,
-                    ),
-                  ),
-                  spacing16Width,
-                  const Expanded(
-                    child: SizedBox(),
-                  ),
-                ],
-              ),
-              spacing20,
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(
-                    child: CommonInputWidget(
-                      label: "Tallas",
-                      hintText: "Talla",
-                      icon: AssetData.iconSize,
-                      controller: sizeController,
-                      inputType: InputTypeEnum.text,
-                    ),
-                  ),
-                  spacing16Width,
-                  InkWell(
-                    onTap: () {
-                      double size = double.parse(sizeController.text);
-                      sizes.add(size);
-                      sizeController.clear();
-                      setState(() {});
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(12.0),
-                      decoration: BoxDecoration(
-                        color: BrandColor.primaryFontColor,
-                        borderRadius: BorderRadius.circular(30.0),
+                  spacing20,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: CommonInputWidget(
+                          label: "Precio (S/.)",
+                          hintText: "Precio",
+                          icon: AssetData.iconDollar,
+                          controller: nameController,
+                          inputType: InputTypeEnum.text,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.add,
-                        color: Colors.white,
+                      spacing16Width,
+                      Expanded(
+                        child: CommonInputWidget(
+                          label: "Descuento (%)",
+                          hintText: "Descuento",
+                          icon: AssetData.iconOffer,
+                          controller: nameController,
+                          inputType: InputTypeEnum.text,
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-              spacing16,
-              Container(
-                height: ResponsiveUI.pDiagonal(context, 0.25),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.05),
-                      blurRadius: 12,
-                      offset: const Offset(4, 4),
-                    ),
-                  ],
-                ),
-                child: sizes.isNotEmpty
-                    ? ListView.builder(
-                        shrinkWrap: true,
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: sizes.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: H5(
-                                text:
-                                    "Talla: ${sizes[sizes.length - index - 1]}"),
-                            trailing: IconButton(
-                              icon: const Icon(
-                                Icons.delete_rounded,
-                                size: 20.0,
-                                color: Colors.redAccent,
-                              ),
-                              onPressed: () {
-                                sizes.removeAt(sizes.length - index - 1);
-                                setState(() {});
-                              },
-                            ),
-                          );
+                  spacing20,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: CommonInputWidget(
+                          label: "Stock",
+                          hintText: "Stock",
+                          icon: AssetData.iconShape,
+                          controller: nameController,
+                          inputType: InputTypeEnum.text,
+                        ),
+                      ),
+                      spacing16Width,
+                      const Expanded(
+                        child: SizedBox(),
+                      ),
+                    ],
+                  ),
+                  spacing20,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: CommonInputWidget(
+                          label: "Tallas",
+                          hintText: "Talla",
+                          icon: AssetData.iconSize,
+                          controller: sizeController,
+                          inputType: InputTypeEnum.text,
+                        ),
+                      ),
+                      spacing16Width,
+                      InkWell(
+                        onTap: () {
+                          double size = double.parse(sizeController.text);
+                          sizes.add(size);
+                          sizeController.clear();
+                          setState(() {});
                         },
-                      )
-                    : Center(child: H5(text: "Aún no hay tallas agregadas.")),
-              ),
-              spacing30,
-              CommonButtonWidget(
-                color: BrandColor.primaryFontColor,
-                text: "Galería",
-                icon: AssetData.iconRocket,
-                onPressed: () {
-                  getImageGallery();
-                },
-              ),
-              spacing20,
-              image != null
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(14.0),
-                      child: Image.file(
-                        File(image!.path),
-                        width: double.infinity,
-                        height: ResponsiveUI.pDiagonal(context, 0.33),
-                        fit: BoxFit.cover,
+                        child: Container(
+                          padding: const EdgeInsets.all(12.0),
+                          decoration: BoxDecoration(
+                            color: BrandColor.primaryFontColor,
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                    )
-                  : const SizedBox(),
-              spacing40,
-            ],
+                    ],
+                  ),
+                  spacing16,
+                  Container(
+                    height: ResponsiveUI.pDiagonal(context, 0.25),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.05),
+                          blurRadius: 12,
+                          offset: const Offset(4, 4),
+                        ),
+                      ],
+                    ),
+                    child: sizes.isNotEmpty
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: sizes.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: H5(
+                                    text:
+                                        "Talla: ${sizes[sizes.length - index - 1]}"),
+                                trailing: IconButton(
+                                  icon: const Icon(
+                                    Icons.delete_rounded,
+                                    size: 20.0,
+                                    color: Colors.redAccent,
+                                  ),
+                                  onPressed: () {
+                                    sizes.removeAt(sizes.length - index - 1);
+                                    setState(() {});
+                                  },
+                                ),
+                              );
+                            },
+                          )
+                        : Center(
+                            child: H5(text: "Aún no hay tallas agregadas.")),
+                  ),
+                  spacing30,
+                  CommonButtonWidget(
+                    color: BrandColor.primaryFontColor,
+                    text: "Galería",
+                    icon: AssetData.iconRocket,
+                    onPressed: () {
+                      getImageGallery();
+                    },
+                  ),
+                  spacing20,
+                  image != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(14.0),
+                          child: Image.file(
+                            File(image!.path),
+                            width: double.infinity,
+                            height: ResponsiveUI.pDiagonal(context, 0.33),
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : const SizedBox(),
+                  spacing40,
+                ],
+              ),
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.all(12.0),
+              child: CommonButtonWidget(
+                text: "Agregar producto",
+                color: BrandColor.secondaryColor,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
