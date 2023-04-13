ce5b9d016be98d021f38ce60767d3e6ef17be1c7
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-04-12 22:58:03 -0500

    Mods

--- lib/ui/pages/admin/reports_admin_page.dart
+++ lib/ui/pages/admin/reports_admin_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shoesappclient/ui/general/brand_color.dart';
 import 'package:shoesappclient/ui/widgets/common_button_widget.dart';
@@ -12,13 +13,14 @@ class ReportAdminPage extends StatelessWidget {
     Excel myExcel = Excel.createExcel();
     Sheet? sheet = myExcel.sheets[myExcel.getDefaultSheet() as String];
     sheet!.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0)).value =
-        "Hola";
+        "Julioooo";
 
     List<int>? bytes = myExcel.save();
     Directory directory = await getApplicationDocumentsDirectory();
     File fileExcel = File("${directory.path}/prueba_excel.xlsx")
       ..createSync(recursive: true)
       ..writeAsBytesSync(bytes!);
+    await OpenFilex.open(fileExcel.path);
   }
 
   @override
